421ec0cac72232f39327b3046f2f53c48e1adffb
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-12-29 15:43:37 +0900

    ViewModel 작성

---
+++ lib/23_12_29/counter_view_model.dart
@@ -0,0 +1,13 @@
+
+class MyHomeViewModel {
+  // repository
+  int _counter = 0;
+
+  int get counter => _counter; // 기능
+
+  void incrementCounter() {
+    _counter++;
+
+    // TODO: 화면 갱신
+  }
+}
