56d5284476dc7aa8f54ff418b7b73531552cc9c5
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-01-02 11:35:39 +0900

    InheritedWidget 적용한 카운터

---
+++ lib/24_01_02/counter.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    CounterProvider(
+      counter: CounterViewModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${CounterProvider.of(context).counter.count}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            CounterProvider.of(context).counter.increment();
+          });
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  final CounterViewModel counter;
+
+  const CounterProvider({
+    super.key,
+    required super.child,
+    required this.counter,
+  });
+
+  static CounterProvider of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<CounterProvider>()!;
+  }
+
+  @override
+  bool updateShouldNotify(CounterProvider oldWidget) {
+    return counter.count != oldWidget.counter.count;
+  }
+}
+
+class CounterViewModel extends ChangeNotifier {
+  var count = 0;
+
+  void increment() {
+    count++;
+    notifyListeners();
+  }
+}
